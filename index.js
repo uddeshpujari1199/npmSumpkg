6e541f5b2ab281ec4007612b6455d774e7573adc
Uddesh Pujari <uddesh.pujari123@gmail.com>
2024-06-14 11:50:34 +0530

    done pkg

---
+++ index.js
@@ -0,0 +1,13 @@
+const add = (a,b)=>a+b;
+const subtract = (a,b)=>a-b;
+const multi = (a,b)=>a*b;
+const div = (a,b)=>a/b;
+
+const calculater={
+    add,
+    subtract,
+    multi,
+    div
+};
+
+module.export= calculater;
